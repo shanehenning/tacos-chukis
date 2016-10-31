9e22f4baf577382a1f16fb7948dab98fec9631e9
Shane Henning <shanerhenning@gmail.com>
2016-10-30 23:17:52 -0700

    slick headaches

--- app/scripts/slider2.js
+++ app/scripts/slider2.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   const preview = $('.preview');
   const flipLeft = $('.flip-left');
   const flipRight = $('.flip-right');
-  const dotsStyle = $('.dot');
+  const dot = $('.dot');
   const miniLeft = $('.mini-nav .fa-chevron-left');
   const miniRight = $('.mini-nav .fa-chevron-right');
 
@@ -16,49 +16,26 @@ $(document).ready(function() {
       autoplaySpeed: 8000,
       prevArrow: flipLeft,
       nextArrow: flipRight,
-      focusOnSelect: true,
       asNavFor: preview,
-      infinite: true
+      infinite: false,
+      responsive: [{
+        breakpoint: 768,
+        settings: {
+          dots: true,
+          appendDots: dot
+        }
+      }]
     });
   }
 
   function asNav() {
     preview.slick({
       focusOnSelect: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
       asNavFor: slider,
       prevArrow: miniLeft,
       nextArrow: miniRight,
-      infinite: true,
-      variableWidth: true,
-      centerMode: true,
-      responsive: [{
-        breakpoint: 800,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true
-        }
-      }, {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          dotsClass: dotsStyle,
-          appendDots: dotsStyle,
-          vertical: false
-        }
-      }, {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 0,
-          slidesToScroll: 1,
-          dotsClass: dotsStyle,
-          appendDots: dotsStyle,
-          vertical: false
-        }
-      }]
+      infinite: false,
+      variableWidth: true
     });
   }
 
