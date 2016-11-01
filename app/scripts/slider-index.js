a17e697374c636772ee29792d26a5a94b64a057f
Shane Henning <shanerhenning@gmail.com>
2016-11-01 15:48:58 -0700

    slider added to home, scss restructuring for consistency

---
+++ app/scripts/slider-index.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  const slider = $('.slides');
+  const dot = $('.index .billboard .dot');
+  const flipLeft = $('.flip-left');
+  const flipRight = $('.flip-right');
+
+  slide();
+
+  function slide() {
+    slider.slick({
+      autoplay: true,
+      autoplaySpeed: 8000,
+      infinite: true,
+      prevArrow: flipLeft,
+      nextArrow: flipRight,
+      dots: true,
+      dotsClass: 'slick-dots-white',
+      appendDots: dot
+    });
+  }
+
+  $(window).resize(function() {
+    $('.slides').slick('resize');
+  });
+
+  $(window).on('orientationchange', function() {
+    $('.slides').slick('resize');
+  });
+});
