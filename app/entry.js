569d3f8de9ff10c18aeb15b6d55b92d793c46f09
Shane Henning <shanerhenning@gmail.com>
2016-11-03 01:44:55 -0700

    d

--- app/entry.js
+++ app/entry.js
@@ -1,9 +1,9 @@
 'use strict';
 
+require('!!file?name=[name].[ext]!./scripts/index.js');
 require('!!file?name=[name].[ext]!./html/index.html');
 require('./scss/index.scss');
 
 require('!!file?name=[name].[ext]!./html/menu.html');
 require('!!file?name=[name].[ext]!./html/about.html');
 require('!!file?name=[name].[ext]!./html/press.html');
-require('!!file?name=[name].[ext]!./scripts/index.js');
