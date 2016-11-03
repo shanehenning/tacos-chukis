eae63643ca7ba9de990d3c1e43ef392f5c02176f
Shane Henning <shanerhenning@gmail.com>
2016-11-03 01:58:24 -0700

    downloaded jquery

--- app/entry.js
+++ app/entry.js
@@ -1,13 +1,7 @@
 'use strict';
 
-require('!!file?name=[name].[ext]!./scripts/vendor/slick-1.6.0/slick/slick.min.js');
-require('!!file?name=[name].[ext]!./scripts/slider-index.js');
-require('!!file?name=[name].[ext]!./scripts/slider-menu.js');
-require('!!file?name=[name].[ext]!./scripts/slider-press.js');
-require('!!file?name=[name].[ext]!./scripts/maps.js');
 require('!!file?name=[name].[ext]!./html/index.html');
-require('./scripts/vendor/slick-1.6.0/slick/slick.min.js');
-require('./scripts/slider-index.js');
+require('./scripts/index.js');
 require('./scss/index.scss');
 
 require('!!file?name=[name].[ext]!./html/menu.html');
