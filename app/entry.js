8911fcf00bab648f588ef441e91ce7e3f61c0a07
Shane Henning <shanerhenning@gmail.com>
2016-11-03 02:17:19 -0700

    ad

--- app/entry.js
+++ app/entry.js
@@ -1,6 +1,5 @@
 'use strict';
 
-require('./scripts/vendor/jquery-3.1.1.min.js');
 require('./scripts/vendor/slick-1.6.0/slick/slick.min.js');
 require('./scripts/slider-index.js');
 require('./scripts/slider-menu.js');
