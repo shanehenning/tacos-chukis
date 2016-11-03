2131a56caf5f02d2a6ffef24f202c06e8e354042
Shane Henning <shanerhenning@gmail.com>
2016-11-03 02:56:16 -0700

    d

--- app/entry.js
+++ app/entry.js
@@ -1,11 +1,7 @@
 'use strict';
 
 // require('script!./scripts/vendor/jquery-3.1.1.min.js');
-require('script!./scripts/vendor/slick-1.6.0/slick/slick.min.js');
-require('script!./scripts/slider-index.js');
-require('script!./scripts/slider-menu.js');
-require('script!./scripts/slider-press.js');
-require('script!./scripts/maps.js');
+require('./scripts/index.js');
 require('!!file?name=[name].[ext]!./html/index.html');
 require('./scss/index.scss');
 
