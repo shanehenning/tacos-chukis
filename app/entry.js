70da57fd0c2df6ba517434e1ddddd3012af74c43
Shane Henning <shanerhenning@gmail.com>
2016-11-03 01:51:56 -0700

    typo again

--- app/entry.js
+++ app/entry.js
@@ -4,7 +4,7 @@ require('!!file?name=[name].[ext]!./scripts/vendor/slick-1.6.0/slick/slick.min.j
 require('!!file?name=[name].[ext]!./scripts/slider-index.js');
 require('!!file?name=[name].[ext]!./scripts/slider-menu.js');
 require('!!file?name=[name].[ext]!./scripts/slider-press.js');
-require('!1file?name=[name].[ext]!./scripts/maps.js');
+require('!!file?name=[name].[ext]!./scripts/maps.js');
 require('!!file?name=[name].[ext]!./html/index.html');
 require('./scss/index.scss');
 
