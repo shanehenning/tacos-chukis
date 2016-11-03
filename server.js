595036c1777c579f5b363600df6e6ddbcf1bcaae
Shane Henning <shanerhenning@gmail.com>
2016-11-03 01:15:59 -0700

    more tweaks

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
-app.use(express.static('./build')).listen(port, function() {
+app.use(express.static(`${__dirname}/build`)).listen(port, function() {
   console.log('Demo app listening on port ' + port + '!');
 });
 
