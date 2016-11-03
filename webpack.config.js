4e887a0dc5547616c9b1b798bd887a50b41a57ca
Shane Henning <shanerhenning@gmail.com>
2016-11-03 02:15:20 -0700

    jquery in package.json

--- webpack.config.js
+++ webpack.config.js
@@ -54,10 +54,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015']
-        }
+        exclude: /node_modules/
       },
       {
         test: /\.html$/,
