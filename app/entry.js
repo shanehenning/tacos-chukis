2a0ca6529e00bdf450c955e1a7a49db9c070a89d
Shane Henning <shanerhenning@gmail.com>
2016-11-03 01:50:32 -0700

    typo

--- app/entry.js
+++ app/entry.js
@@ -1,10 +1,10 @@
 'use strict';
 
 require('!!file?name=[name].[ext]!./scripts/vendor/slick-1.6.0/slick/slick.min.js');
-require('!!file?name=[name].ext!./scripts/slider-index.js');
-require('!!file?name=[name].ext!./scripts/slider-menu.js');
-require('!!file?name=[name].ext!./scripts/slider-press.js');
-require('!1file?name=[name].ext!./scripts/maps.js');
+require('!!file?name=[name].[ext]!./scripts/slider-index.js');
+require('!!file?name=[name].[ext]!./scripts/slider-menu.js');
+require('!!file?name=[name].[ext]!./scripts/slider-press.js');
+require('!1file?name=[name].[ext]!./scripts/maps.js');
 require('!!file?name=[name].[ext]!./html/index.html');
 require('./scss/index.scss');
 
