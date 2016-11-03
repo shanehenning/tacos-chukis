9b5429d36e8fcc7ae435c859e7cc5a2be0e2f0ed
Shane Henning <shanerhenning@gmail.com>
2016-11-03 00:53:28 -0700

    send file

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
-var express = require('express');
+const express = require('express');
+const app = express();
 
 const port = process.env.PORT || 8080;
 
-express().use(express.static('./build')).listen(port, function() {
+app.use(express.static('./build')).listen(port, function() {
   console.log('Demo app listening on port ' + port + '!');
+});
+
+app.get('*', function(req, res) {
+  res.sendfile('${__dirname}/public/index/html');
 });
